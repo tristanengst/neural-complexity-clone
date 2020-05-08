68f39cfc8eede383a0a701b1a139a87e0834bd33
Tristan Engst <tristanengst@gmail.com>
2020-05-08 06:00:28 -0400

    testing surprisals

--- main.py
+++ main.py
@@ -360,7 +360,7 @@ def get_complexity(state, obs, sentid):
             # print(args.csep.join([str(word), str(sentid), str(corpuspos), str(len(word)),
             #                       str(float(surp)), str(float(Hs[corpuspos])),
             #                       str(max(0, float(Hs[max(corpuspos-1, 0)])-float(Hs[corpuspos])))]))
-    print("Surprisals", torch.sum(surps.tolist() / len(surps), len(surps))
+    print("Surprisals", sum(surps.tolist() / len(surps), len(surps))
 
 def apply(func, apply_dimension):
     ''' Applies a function along a given dimension '''
